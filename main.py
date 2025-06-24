9887f5058eb31fb017f76acc998ca40ecae7ccff
Sam <sl10015@nyu.edu>
2025-06-24 19:22:46 -0400

    Completed the while loop accumulator

--- main.py
+++ main.py
@@ -17,19 +17,20 @@ def main():
     - calls the get_starting_number function to determine how many bottles of beer to start with.
     - and then call the sing method, passing the number of bottles to start as its argument.
     """
-
+    """
     # run for loop version
     num_bottles = flv.get_starting_number()
     flv.sing(num_bottles)
-"""
+    """
     # run while loop with accumulator version
     num_bottles = wlav.get_starting_number()
     wlav.sing(num_bottles)
-
+    """
     # run while loop with flag version
     num_bottles = wlfv.get_starting_number()
     wlfv.sing(num_bottles)
+    """
 
-"""
+
 # call main
 main()
---
+++ while_loop_accumulator_version.py
@@ -0,0 +1,36 @@
+def get_starting_number():
+
+    while True:
+
+        user_input = input("How many bottles of beer on the wall? ")
+        if user_input.isdigit() and int(user_input) >= 1:
+            return int(user_input)
+    
+def sing(num_bottles):
+    current_bottle = num_bottles
+    while current_bottle > 0:
+        if current_bottle == 1:
+            bottle_form = "bottle"
+        else:
+            bottle_form = "bottles"
+
+        
+        next_bottle = current_bottle - 1
+
+        if next_bottle == 1:
+            next_bottle_form = "bottle"
+        else:
+            next_bottle_form = "bottles"
+
+        print(f"{current_bottle} {bottle_form} of beer on the wall, {current_bottle} {bottle_form} of beer.")
+
+        if next_bottle == 0:
+            print("Take it down, pass it around, no more bottles of beer on the wall!")
+        else:
+            if current_bottle == 1:
+                print(f"Take it down, pass it around, {next_bottle} {bottle_form} of beer on the wall.")
+            else: 
+                print(f"Take one down, pass it around, {next_bottle} {next_bottle_form} of beer on the wall.")
+        current_bottle = current_bottle - 1
+
+        print() 
